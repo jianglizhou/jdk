1a045726cced9d00a11743aa205093d1724327b0
Jiangli Zhou <jianglizhou@google.com>
2023-07-26 15:48:47 -0700

    Fix crash with jtreg tests on dynamic JDK: - Add JLI_SetStaticJDK in libjli and set_static_jdk/is_static_jdk in libjvm. JLI_SetStaticJDK is called before loading and creating the JVM. It checks if execution using static build by looking up the hotspot set_static_jdk. set_static_jdk is called if found. - Fix VM code to use set_static_jdk in a few places. Earlier symbol lookup was used in those cases. However the requested symbols may have been loaded already and the result cannot determine if it's running on static JDK. - Rename IsStaticJDK back to JLI_IsStaticJDK.

--- src/hotspot/share/classfile/classLoader.cpp
+++ src/hotspot/share/classfile/classLoader.cpp
@@ -920,8 +920,9 @@ void* ClassLoader::dll_lookup(void* lib, const char* name, const char* path) {
 
 void ClassLoader::load_java_library() {
   assert(CanonicalizeEntry == nullptr, "should not load java library twice");
-  CanonicalizeEntry = CAST_TO_FN_PTR(canonicalize_fn_t, os::lookup_function("JDK_Canonicalize"));
-  if (CanonicalizeEntry != nullptr) {
+  if (is_static_jdk()) {
+    CanonicalizeEntry = CAST_TO_FN_PTR(canonicalize_fn_t, os::lookup_function("JDK_Canonicalize"));
+    assert(CanonicalizeEntry != nullptr, "no JDK_Canonicalize");
     return;
   }
 
@@ -943,11 +944,9 @@ void ClassLoader::release_load_zip_library() {
 
 void ClassLoader::load_zip_library() {
   assert(ZipOpen == nullptr, "should not load zip library twice");
-  char path[JVM_MAXPATHLEN];
-  char ebuf[1024];
 
   // Check if we are running on a static build.
-  if (os::lookup_function("ZIP_Open") != nullptr) {
+  if (is_static_jdk()) {
     ZipOpen = CAST_TO_FN_PTR(ZipOpen_t, os::lookup_function("ZIP_Open"));
     ZipClose = CAST_TO_FN_PTR(ZipClose_t, os::lookup_function("ZIP_Close"));
     FindEntry = CAST_TO_FN_PTR(FindEntry_t, os::lookup_function("ZIP_FindEntry"));
@@ -955,6 +954,9 @@ void ClassLoader::load_zip_library() {
     Crc32 = CAST_TO_FN_PTR(Crc32_t, os::lookup_function("ZIP_CRC32"));
     return;
   }
+
+  char path[JVM_MAXPATHLEN];
+  char ebuf[1024];
 
   if (os::dll_locate_lib(path, sizeof(path), Arguments::get_dll_dir(), "zip")) {
     _zip_handle = os::dll_load(path, ebuf, sizeof ebuf);
@@ -972,18 +974,19 @@ void ClassLoader::load_zip_library() {
 
 void ClassLoader::load_jimage_library() {
   assert(JImageOpen == nullptr, "should not load jimage library twice");
-  char path[JVM_MAXPATHLEN];
-  char ebuf[1024];
-  void* handle = nullptr;
 
   // Check if we are running on a static build.
-  if (os::lookup_function("JIMAGE_Open") != nullptr) {
+  if (is_static_jdk()) {
     JImageOpen = CAST_TO_FN_PTR(JImageOpen_t, os::lookup_function("JIMAGE_Open"));
     JImageClose = CAST_TO_FN_PTR(JImageClose_t, os::lookup_function("JIMAGE_Close"));
     JImageFindResource = CAST_TO_FN_PTR(JImageFindResource_t, os::lookup_function("JIMAGE_FindResource"));
     JImageGetResource = CAST_TO_FN_PTR(JImageGetResource_t, os::lookup_function("JIMAGE_GetResource"));
     return;
   }
+
+  char path[JVM_MAXPATHLEN];
+  char ebuf[1024];
+  void* handle = nullptr;
 
   if (os::dll_locate_lib(path, sizeof(path), Arguments::get_dll_dir(), "jimage")) {
     handle = os::dll_load(path, ebuf, sizeof ebuf);
